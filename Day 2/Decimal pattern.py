b0f907b47c67252ef411dbe9f32857a064c41782
KESAVAN007S <110806029+KESAVAN007S@users.noreply.github.com>
2023-02-05 14:02:35 +0530

    Update Decimal pattern.py

--- Day 2/Decimal pattern.py
+++ Day 2/Decimal pattern.py
@@ -1,6 +1,4 @@
-Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> a=float(input("Enter the number: ")) 
+a=float(input("Enter the number: ")) 
  b=int(input("Enter the number of rows: ")) 
  for i in range(b): 
      for j in range(i+1): 
