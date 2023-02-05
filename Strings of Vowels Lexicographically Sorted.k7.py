c12219467ed451f1abae53d973138fd5d596d1a4
KESAVAN007S <110806029+KESAVAN007S@users.noreply.github.com>
2023-02-05 13:59:23 +0530

    Update Strings of Vowels Lexicographically Sorted.k7.py

--- Strings of Vowels Lexicographically Sorted.k7.py
+++ Strings of Vowels Lexicographically Sorted.k7.py
@@ -1,15 +1,13 @@
-def solve(n):
-   if n==1:
-      return 5
-   count = [1 for i in range(6)]
-   for i in range(3,n+1):
-      count[1] = count[1]+count[2]+count[3]+count[4]+count[5]
-      count[2] = count[2]+count[3]+count[4]+count[5]
-      count[3] = count[3]+count[4]+count[5]
-      count[4] = count[4]+count[5]
-   total = 0
-   for i in range(1,6):
-      total += i*count[i]
-   return total
-n = int(input("Enter the value of n:"))
-print(solve(n))
+def countstring(n, start):
+    if n==0:
+        return 1
+    count=0
+    for i in range(start, 5):
+        count+=countstring(n-1, i)
+    return count
+
+def countvowel(n):
+    return countstring(n,0)
+
+n=int(input("Enter the length of the string: "))
+print(countvowel(n))
