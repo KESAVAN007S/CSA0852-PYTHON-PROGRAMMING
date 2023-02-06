79be8698029e7c9800fa51ef72cc93d7da8cb312
KESAVAN007S <110806029+KESAVAN007S@users.noreply.github.com>
2023-02-06 08:37:10 +0530

    Update and rename Combination.py to DAY03/Combination.py

--- DAY03/Combination.py
+++ DAY03/Combination.py
@@ -1,6 +1,4 @@
-Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> def combination(l):
+def combination(l):
     for i in range(3):
         for j in range(3):
             for k in range(3):
